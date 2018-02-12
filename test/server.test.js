40f4635bb101c7730da5fd3d93ff69a8f53ccb15
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-02-11 19:24:11 -0800

    Work

--- test/server.test.js
+++ test/server.test.js
@@ -92,11 +92,11 @@ describe('Environment', () => {
   it('should search by folder id', function () {
     const dataPromise = knex.select()
       .from('notes')
-      .where('folder_id', 103)
+      .where('folder_id', 102)
       .orderBy('notes.id');
 
     const apiPromise = chai.request(app)
-      .get('/v2/notes?folderId=103');
+      .get('/v2/notes?folderId=102');
 
     return Promise.all([dataPromise, apiPromise])
       .then(function ([data, res]) {
@@ -196,7 +196,7 @@ describe('GET /v2/notes', function () {
 
   it('should return correct search results for a valid query', function () {
     return chai.request(app)
-      .get('/v2/notes?searchTerm=5%20life')
+      .get('/v2/notes?searchTerm=5%20cats')
       .then(function (res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -483,60 +483,3 @@ describe('Delete a folder', function () {
   });
 
 });
-// describe('PUT /v2/notes/:id', function () {
-
-//   it('should update the note', function () {
-//     const updateItem = {
-//       'title': 'What about dogs?!',
-//       'content': 'woof woof',
-//       'tags': []
-//     };
-//     return chai.request(app)
-//       .put('/v2/notes/1005')
-//       .send(updateItem)
-//       .then(function (res) {
-//         expect(res).to.have.status(200);
-//         expect(res).to.be.json;
-//         expect(res.body).to.be.a('object');
-//         expect(res.body).to.include.keys('id', 'title', 'content');
-
-//         expect(res.body.id).to.equal(1005);
-//         expect(res.body.title).to.equal(updateItem.title);
-//         expect(res.body.content).to.equal(updateItem.content);
-//       });
-//   });
-
-//   it('should return correct search results for a searchTerm query', function () {
-//     let res;
-//     return chai.request(app).get('/v2/notes?searchTerm=gaga')
-//       .then(function (_res) {
-//         res = _res;
-//         expect(res).to.have.status(200);
-//         expect(res).to.be.json;
-//         expect(res.body).to.be.a('array');
-//         expect(res.body).to.have.length(1);
-//         expect(res.body[0]).to.be.an('object');
-//         return knex.select().from('notes').where('title', 'like', '%gaga%');
-//       })
-//       .then(data => {
-//         expect(res.body[0].id).to.equal(data[0].id);
-//       });
-//   });
-
-//   // it('should return the default of 10 Notes ', function () {
-//   //   let count;
-//   //   return knex.count()
-//   //     .from('notes')
-//   //     .then(([result]) => {
-//   //       count = Number(result.count);
-//   //       return chai.request(app).get('/v2/notes');
-//   //     })
-//   //     .then(function (res) {
-//   //       expect(res).to.have.status(200);
-//   //       expect(res).to.be.json;
-//   //       expect(res.body).to.be.a('array');
-//   //       expect(res.body).to.have.length(count);
-//   //     });
-//   // });
-  
-// });
